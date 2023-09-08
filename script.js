860a84470ad65397ba2508733f024d64b9d90048
Chinwe Onumonu <kaforglo122@yahoo.com>
2023-09-08 09:55:33 +0100

    create another function called playRound. Add two permeters playSelection and computerSelection to the function

--- script.js
+++ script.js
@@ -1,6 +1,20 @@
+const prompt = require("prompt-sync")();
 function getComputerChoice() {
     const gameChoice = ["Rock" , "Paper" , "Scissors"];
-    console.log(gameChoice);
+    let choices = Math.floor(Math.random() * gameChoice.length);
+    return gameChoice[choices]
 
 }
-getComputerChoice();
+// getComputerChoice();
+
+function playRound(playerSelection, computerSelection) {
+    let input = prompt("Enter Your chioice game: Rock, Paper, Scissors");
+    if (playerSelection === "rock" && computerSelection === "Paper");
+    return "You Lose! Paper beats Rocks";
+
+}
+
+
+const playerSelection = "rock";
+const computerSelection = getComputerChoice();
+console.log(playRound(playerSelection, computerSelection));
